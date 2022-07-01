98294d6fd061ad33ca56b34ac5690473bc05adcf
PTThanh <ptthanh@ptcon.com.vn>
2022-07-01 14:36:58 +0700

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IFC_Regenerate
+{
+    public class Class1
+    {
+    }
+}
