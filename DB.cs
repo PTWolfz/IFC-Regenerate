70c7ba862d18671a91bc2e5e45fb7950f87d5fe3
PTThanh <ptthanh@ptcon.com.vn>
2022-07-06 11:32:02 +0700

    Released. *Lack of beams

--- DB.cs
+++ DB.cs
@@ -8,6 +8,120 @@ using System.Threading.Tasks;
 
 namespace IFC_Regenerate
 {
+    public class IfcFile
+    {
+        Regex regexNo = new Regex(@"\d*\b");
+        Regex regexIFC = new Regex(@"\bIFC\w*");
+        Regex regexDecimal = new Regex(@"-*?\d*?\.\d*");
+        public IfcFile(string filePath, Document doc)
+        {
+            FilePath=filePath;
+            IfcLines=new List<IfcLine>();
+            string ifcString = System.IO.File.ReadAllText(filePath);
+            string ifcData = ifcString.Split(new string[] { "DATA;\r\n" }, StringSplitOptions.None)[1];
+            List<string> ifcLineContents = ifcData.Split(new string[] { ";\r\n" }, StringSplitOptions.None).ToList();
+            foreach (string ifcLineContent in ifcLineContents)
+            {
+                if (ifcLineContent.Length>0&&ifcLineContent.Substring(0, 1)==@"#")
+                {
+                    IfcLine ifcLine = new IfcLine(ifcLineContent);
+                    IfcLines.Add(ifcLine);
+                }
+            }
+            IfcElements=getIfcElements();
+            assignLevel(IfcElements, doc);
+        }
+        List<IfcElement> getIfcElements()
+        {
+            List<IfcElement> ifcElements = new List<IfcElement>();
+            foreach (IfcLine ifcLine in IfcLines)
+            {
+                if (ifcLine.IfcTypeName=="IFCSLAB")
+                {
+                    IfcElement ifcElem = new IfcElement(ifcLine);
+                    ifcElem.Category="Structural Foundations";
+                    ifcElem.ECategory=ECategory.StructuralFoundations;
+                    ifcElements.Add(ifcElem);
+                }
+                else if (ifcLine.IfcTypeName=="IFCCOLUMN")
+                {
+                    IfcElement ifcElem = new IfcElement(ifcLine);
+                    ifcElem.Category="Structural Columns";
+                    ifcElem.ECategory=ECategory.StructuralColumns;
+                    ifcElements.Add(ifcElem);
+                }
+                else if (ifcLine.IfcTypeName=="IFCBEAM")
+                {
+                    IfcElement ifcElem = new IfcElement(ifcLine);
+                    ifcElem.Category="Structural Framing";
+                    ifcElem.ECategory=ECategory.StructuralFraming;
+                    ifcElements.Add(ifcElem);
+                }
+            }
+            return ifcElements;
+        }
+        void assignLevel(List<IfcElement> ifcElements, Document doc)
+        {
+            List<Level> levels = new FilteredElementCollector(doc).OfClass(typeof(Level)).Cast<Level>().ToList();
+            Regex regexSharpNo = new Regex(@"#\d*\b");
+            List<IfcLine> spatialContents = IfcLines.Where(x => x.IfcTypeName=="IFCRELCONTAINEDINSPATIALSTRUCTURE").ToList();
+            foreach (string spatialContent in spatialContents.Select(x => x.FullLine))
+            {
+                List<string> lineNumbers = regexSharpNo.Matches(spatialContent)._ToListString().Select(x => x.Substring(1, x.Length-1)).ToList();
+                string levelNo = lineNumbers.Last();
+                string levelContent = IfcLines.First(x => x.LineNo.ToString()==levelNo).FullLine;
+                Level level = levels.First(x => x.Name==levelContent.Split('\'')[3]);
+                for (int i = 2; i<lineNumbers.Count()-1; i++)
+                {
+                    string lineNumber = lineNumbers[i];
+                    string objectContent = IfcLines.First(x => x.LineNo.ToString()==lineNumber).FullLine;
+                    string objectId = objectContent.Split('\'')[7];
+                    try
+                    {
+                        ifcElements.First(x => x.ElementId.ToString()==objectId).Level=level;
+                    }
+                    catch (Exception)
+                    {
+                    }
+                }
+            }
+        }
+        public List<IfcElement> IfcElements;
+        public static IfcLine ContentAtIndex(int index)
+        {
+            return IfcLines.FirstOrDefault(x => x.LineNo == index);
+        }
+        string filePath;
+        public string FilePath { get { return filePath; } set { filePath = value; } }
+        public static List<IfcLine> IfcLines;
+    }
+    public enum ECategory
+    {
+        StructuralFoundations,
+        StructuralColumns,
+        StructuralFraming
+    }
+    public class IfcLine
+    {
+        public IfcLine(string input)
+        {
+            Regex regexNo = new Regex(@"\d*\b");
+            Regex regexSharpNo = new Regex(@"#\d*\b");
+            Regex regexIFC = new Regex(@"\bIFC\w*");
+            FullLine=input;
+            LineNo=Int32.Parse(regexNo.Match(input).ToString());
+            IfcTypeName=regexIFC.Match(input).ToString();
+            LinesNo=regexSharpNo.Matches(input)._ToListString().Select(x => Int32.Parse(x.Substring(1, x.Length-1))).ToList();
+        }
+        string fullLine;
+        public string FullLine { get { return fullLine; } set { fullLine = value; } }
+        string ifcTypeName;
+        public string IfcTypeName { get { return ifcTypeName; } set { ifcTypeName = value; } }
+        int lineNo;
+        public int LineNo { get { return lineNo; } set { lineNo = value; } }
+        List<int> linesNo;
+        public List<int> LinesNo { get { return linesNo; } set { linesNo = value; } }
+    }
     public class IfcElement
     {
         public IfcElement(string typeName, string familyName)
@@ -15,13 +129,18 @@ namespace IFC_Regenerate
             TypeName = typeName;
             FamilyName = familyName;
         }
-        public IfcElement(string ifcLine)
+        public IfcElement(IfcLine ifcLine)
         {
-            string ifcInfo = ifcLine.Split('\'')[3];
+            string ifcInfo = ifcLine.FullLine.Split('\'')[3];
             TypeName = ifcInfo.Split(':')[1];
             FamilyName = ifcInfo.Split(':')[0];
             ElementId = int.Parse(ifcInfo.Split(':')[2]);
+            LineNo=ifcLine.LineNo;
+            IfcLocalPlacement = new IfcLocalPlacement(IfcFile.ContentAtIndex(ifcLine.LinesNo[2]));
+            IfcProductDefinitionShape=new IfcProductDefinitionShape(IfcFile.ContentAtIndex(ifcLine.LinesNo[3]));
         }
+        int lineNo;
+        public int LineNo { get { return lineNo; } set { lineNo = value; } }
         string typeName;
         public string TypeName { get { return typeName; } set { typeName = value; } }
         string familyName;
@@ -30,11 +149,22 @@ namespace IFC_Regenerate
         public int ElementId { get { return elementId; } set { elementId = value; } }
         string category;
         public string Category { get { return category; } set { category = value; } }
+        ECategory eCategory;
+        public ECategory ECategory { get { return eCategory; } set { eCategory = value; } }
         Level level;
         public Level Level { get { return level; } set { level = value; } }
+        IfcLocalPlacement ifcLocalPlacement;
+        public IfcLocalPlacement IfcLocalPlacement { get { return ifcLocalPlacement; } set { ifcLocalPlacement = value; } }
+        IfcProductDefinitionShape ifcProductDefinitionShape;
+        public IfcProductDefinitionShape IfcProductDefinitionShape { get { return ifcProductDefinitionShape; } set { ifcProductDefinitionShape = value; } }
     }
     public class IfcLocalPlacement
     {
+        public IfcLocalPlacement(IfcLine ifcLine)
+        {
+            LineNo=ifcLine.LineNo;
+            IfcAxis2Placement3D=new IfcAxis2Placement3D(IfcFile.ContentAtIndex(ifcLine.LinesNo[2]));
+        }
         int lineNo;
         public int LineNo { get { return lineNo; } set { lineNo = value; } }
         IfcLocalPlacement ifcLPlacement;
@@ -44,10 +174,11 @@ namespace IFC_Regenerate
     }
     public class IfcAxis2Placement3D
     {
-        public IfcAxis2Placement3D(string ifcLine)
+        public IfcAxis2Placement3D(IfcLine ifcLine)
         {
             Regex regexNo = new Regex(@"\d*");
-            LineNo = int.Parse(regexNo.Match(ifcLine).ToString());
+            LineNo = ifcLine.LineNo;
+            IfcCatesianPoint=new IfcCatesianPoint(IfcFile.ContentAtIndex(ifcLine.LinesNo[1]));
         }
         int lineNo;
         public int LineNo { get { return lineNo; } set { lineNo = value; } }
@@ -56,13 +187,21 @@ namespace IFC_Regenerate
     }
     public class IfcCatesianPoint
     {
-        public IfcCatesianPoint(string ifcLine)
+        public IfcCatesianPoint(IfcLine ifcLine)
         {
             Regex regexNo = new Regex(@"\d*");
-            LineNo = int.Parse(regexNo.Match(ifcLine).ToString());
+            LineNo = ifcLine.LineNo;
             Regex regexDecimal = new Regex(@"-*?\d*?\.\d*");
-            List<double> coordinates = regexDecimal.Matches(ifcLine)._ToListString().Select(x => double.Parse(x)).ToList();
-            XYZ = new XYZ(coordinates[0], coordinates[1], coordinates[2]);
+            List<double> coordinates = regexDecimal.Matches(ifcLine.FullLine)._ToListString().Select(x => double.Parse(x)).ToList();
+            if (coordinates.Count==3)
+            {
+                XYZ = new XYZ(coordinates[0], coordinates[1], coordinates[2]);
+            }
+            else if (coordinates.Count==2)
+            {
+                UV = new UV(coordinates[0], coordinates[1]);
+            }
+
         }
         int lineNo;
         public int LineNo { get { return lineNo; } set { lineNo = value; } }
@@ -72,6 +211,91 @@ namespace IFC_Regenerate
             get { return xyz; }
             set { xyz = value; }
         }
+        UV uv;
+        public UV UV
+        {
+            get { return uv; }
+            set { uv = value; }
+        }
+    }
+    public class IfcProductDefinitionShape
+    {
+        public IfcProductDefinitionShape(IfcLine ifcLine)
+        {
+            LineNo = ifcLine.LineNo;
+            IfcShapeRepresentaion=new IfcShapeRepresentation(IfcFile.ContentAtIndex(ifcLine.LinesNo[1]));
+        }
+        int lineNo;
+        public int LineNo { get { return lineNo; } set { lineNo = value; } }
+        IfcShapeRepresentation ifcShapeRepresentaion;
+        public IfcShapeRepresentation IfcShapeRepresentaion { get { return ifcShapeRepresentaion; } set { ifcShapeRepresentaion = value; } }
+    }
+    enum EIfcShapeRepresentation
+    {
+        SweptSolid,
+        MappedRepresentation
+    }
+    public class IfcShapeRepresentation
+    {
+        public IfcShapeRepresentation(IfcLine ifcLine)
+        {
+            LineNo = ifcLine.LineNo;
+            Regex text = new Regex(@"\w*\b");
+            string type = text.Matches(ifcLine.FullLine)._ToListString()[4];
+            if (type==EIfcShapeRepresentation.SweptSolid.ToString())
+            {
+                IfcExtrudedAreaSolid=new IfcExtrudedAreaSolid(IfcFile.ContentAtIndex(ifcLine.LinesNo[2]));
+            }
+            else if (type==EIfcShapeRepresentation.MappedRepresentation.ToString())
+            {
+                IfcMappedItem=new IfcMappedItem(IfcFile.ContentAtIndex(ifcLine.LinesNo[2]));
+            }
+        }
+        int lineNo;
+        public int LineNo { get { return lineNo; } set { lineNo = value; } }
+        IfcMappedItem ifcMappedItem;
+        public IfcMappedItem IfcMappedItem { get { return ifcMappedItem; } set { ifcMappedItem = value; } }
+        IfcExtrudedAreaSolid ifcExtrudedAreaSolid;
+        public IfcExtrudedAreaSolid IfcExtrudedAreaSolid { get { return ifcExtrudedAreaSolid; } set { ifcExtrudedAreaSolid = value; } }
+    }
+    public class IfcMappedItem
+    {
+        public IfcMappedItem(IfcLine ifcLine)
+        {
+            LineNo = ifcLine.LineNo;
+            IfcRepresentaionMap=new IfcRepresentaionMap(IfcFile.ContentAtIndex(ifcLine.LinesNo[1]));
+        }
+        int lineNo;
+        public int LineNo { get { return lineNo; } set { lineNo = value; } }
+        IfcRepresentaionMap ifcRepresentaionMap;
+        public IfcRepresentaionMap IfcRepresentaionMap { get { return ifcRepresentaionMap; } set { ifcRepresentaionMap = value; } }
+    }
+    public class IfcExtrudedAreaSolid
+    {
+        public IfcExtrudedAreaSolid(IfcLine ifcLine)
+        {
+            LineNo = ifcLine.LineNo;
+            IfcAxis2Placement3D=new IfcAxis2Placement3D(IfcFile.ContentAtIndex(ifcLine.LinesNo[2]));
+        }
+        int lineNo;
+        public int LineNo { get { return lineNo; } set { lineNo = value; } }
+        IfcAxis2Placement3D ifcAxis2Placement3D;
+        public IfcAxis2Placement3D IfcAxis2Placement3D { get { return ifcAxis2Placement3D; } set { ifcAxis2Placement3D = value; } }
+    }    
+    public class IfcRepresentaionMap
+    {
+        public IfcRepresentaionMap(IfcLine ifcLine)
+        {
+            LineNo = ifcLine.LineNo;
+            IfcAxis2Placement3D=new IfcAxis2Placement3D(IfcFile.ContentAtIndex(ifcLine.LinesNo[1]));
+            IfcShapeRepresentation=new IfcShapeRepresentation(IfcFile.ContentAtIndex(ifcLine.LinesNo[2]));
+        }
+        int lineNo;
+        public int LineNo { get { return lineNo; } set { lineNo = value; } }
+        IfcAxis2Placement3D ifcAxis2Placement3D;
+        public IfcAxis2Placement3D IfcAxis2Placement3D { get { return ifcAxis2Placement3D; } set { ifcAxis2Placement3D = value; } }
+        IfcShapeRepresentation ifcShapeRepresentation;
+        public IfcShapeRepresentation IfcShapeRepresentation { get { return ifcShapeRepresentation; } set { ifcShapeRepresentation = value; } }
     }
     public class IfcSlab : IfcElement
     {
--- IFCRegenerate.cs
+++ IFCRegenerate.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
+using System.Windows.Forms;
 
 namespace IFC_Regenerate
 {
@@ -23,22 +24,23 @@ namespace IFC_Regenerate
             Autodesk.Revit.ApplicationServices.Application app = uiapp.Application;
             Autodesk.Revit.DB.Document doc = uidoc.Document;
 
-            //string filePath = @"D:\PTT\05. Download\Document Test\project.ifc";
-            string filePath = @"D:\Download\Document Test\project.ifc";
-            List<IfcElement> ifcElements = ifcFileAnalyst(filePath);
-            assignLevel(ifcElements, filePath, doc);
+            string filePath = @"D:\PTT\05. Download\Document Test\project.ifc";
+
+            IfcFile ifcFile = new IfcFile(filePath, doc);
+            List<IfcElement> ifcElements = ifcFile.IfcElements;
+
             using (Transaction regenerateTrans = new Transaction(doc, "Ifc Regenerate"))
             {
                 regenerateTrans.Start();
                 foreach (IfcElement ifcElem in ifcElements)
                 {
-                    if (ifcElem is IfcSlab)
+                    if (ifcElem.ECategory==ECategory.StructuralFoundations||ifcElem is IfcSlab)
                     {
-                        createFoundation(ifcElem as IfcSlab, doc);
+                        createFoundation(ifcElem, doc);
                     }
-                    else if (ifcElem is IfcColumn)
+                    else if (ifcElem.ECategory==ECategory.StructuralColumns||ifcElem is IfcColumn)
                     {
-                        createColumn(ifcElem as IfcColumn, doc);
+                        createColumn(ifcElem , doc);
                     }
                     else if (ifcElem is IfcBeam)
                     {
@@ -49,150 +51,28 @@ namespace IFC_Regenerate
             }
             return Result.Succeeded;
         }
-        List<IfcElement> ifcFileAnalyst(string filePath)
-        {
-            List<IfcElement> ifcElements = new List<IfcElement>();
-            string ifcString = System.IO.File.ReadAllText(filePath);
-            string ifcData = ifcString.Split(new string[] { "DATA;\r\n" }, StringSplitOptions.None)[1];
-            List<string> ifcLines = ifcData.Split(new string[] { ";\r\n" }, StringSplitOptions.None).ToList();
-
-            int i = 0;
-            while (i<ifcLines.Count())
-            {
-                string ifcContent = ifcLines[i];
-                if (regexIFC.Match(ifcContent).ToString()=="IFCSLAB")
-                {
-                    int j = i;
-                    XYZ coordinate;
-                    while (true)
-                    {
-                        j--;
-                        string slabContent = ifcLines[j];
-                        if (regexIFC.Match(slabContent).ToString()=="IFCAXIS2PLACEMENT3D")
-                        {
-                            //Skip if not suitable
-                            if (regexNo.Matches(slabContent)._ToListString().Count!=4) { continue; }
-
-                            string coordinateNo = regexNo.Matches(slabContent)._ToListString()[1].ToString();
-
-                            string coordinateContent = ifcLines.First(
-                              x => regexNo.Match(x).ToString()==coordinateNo);
-
-
-                            coordinate=new XYZ(double.Parse(regexDecimal.Matches(coordinateContent)[0].ToString())._MmToFeet(),
-                                double.Parse(regexDecimal.Matches(coordinateContent)[1].ToString())._MmToFeet(),
-                               0);
-                            break;
-                        }
-                    }
-                    string ifcInfo = ifcContent.Split('\'')[3];
-                    IfcSlab ifcSlab = new IfcSlab(int.Parse(ifcInfo.Split(':')[2]), ifcInfo.Split(':')[1],
-                        ifcInfo.Split(':')[0], coordinate);
-                    ifcElements.Add(ifcSlab);
-                    i++;
-                }
-                else if (regexIFC.Match(ifcContent).ToString()=="IFCCOLUMN")
-                {
-                    XYZ coordinate = null;
-                    double height = 0;
-                    int j = i;
-                    while (true)
-                    {
-                        j--;
-                        string slabContent = ifcLines[j];
-                        if (coordinate==null&&regexIFC.Match(slabContent).ToString()=="IFCAXIS2PLACEMENT3D")
-                        {
-                            //Skip if not suitable
-                            if (regexNo.Matches(slabContent)._ToListString().Count!=4) { continue; }
-                            //if (regexNo.Matches(slabContent)._ToListString()[1]=="6") { continue; }
-
-                            string coordinateNo = regexNo.Matches(slabContent)._ToListString()[1].ToString();
-
-                            string coordinateContent = ifcLines.First(
-                              x => regexNo.Match(x).ToString()==coordinateNo);
-
-
-
-                            coordinate=new XYZ(double.Parse(regexDecimal.Matches(coordinateContent)[0].ToString())._MmToFeet(),
-                                double.Parse(regexDecimal.Matches(coordinateContent)[1].ToString())._MmToFeet(),
-                               0);
-                        }
-                        else if (height==0&&regexIFC.Match(slabContent).ToString()=="IFCEXTRUDEDAREASOLID")
-                        {
-                            height=double.Parse(regexDecimal.Match(slabContent).ToString());
-                        }
-                        if (coordinate!=null&&height!=0)
-                        {
-                            break;
-                        }
-                    }
-                    string ifcInfo = ifcContent.Split('\'')[3];
-                    IfcColumn ifcColumn = new IfcColumn(int.Parse(ifcInfo.Split(':')[2]), ifcInfo.Split(':')[1],
-                        ifcInfo.Split(':')[0], coordinate, height);
-                    ifcElements.Add(ifcColumn);
-                    i++;
-                }
-                else if (regexIFC.Match(ifcContent).ToString()=="IFCBEAM")
-                {
-                    i++;
-                }
-                else
-                {
-                    i++;
-                }
-            }
-            return ifcElements;
-        }
-        void assignLevel(List<IfcElement> ifcElements, string filePath, Document doc)
-        {
-            string ifcString = System.IO.File.ReadAllText(filePath);
-            string ifcData = ifcString.Split(new string[] { "DATA;\r\n" }, StringSplitOptions.None)[1];
-            List<string> ifcLines = ifcData.Split(new string[] { ";\r\n" }, StringSplitOptions.None).ToList();
-
-            List<Level> levels = new FilteredElementCollector(doc).OfClass(typeof(Level)).Cast<Level>().ToList();
-            Regex regexSharpNo = new Regex(@"#\d*\b");
-            List<string> spatialContents = ifcLines.Where(x => regexIFC.Match(x).ToString()=="IFCRELCONTAINEDINSPATIALSTRUCTURE").ToList();
-            foreach (string spatialContent in spatialContents)
-            {
-                List<string> lineNumbers = regexSharpNo.Matches(spatialContent)._ToListString().Select(x => x.Substring(1, x.Length-1)).ToList();
-                string levelNo = lineNumbers.Last();
-                string levelContent = ifcLines.First(x => regexNo.Match(x).ToString()==levelNo);
-                Level level = levels.First(x => x.Name==levelContent.Split('\'')[3]);
-                for(int i=2;i<lineNumbers.Count()-1;i++)
-                {
-                    string lineNumber = lineNumbers[i];
-                    string objectContent = ifcLines.First(x => regexNo.Match(x).ToString()==lineNumber);
-                    string objectId= objectContent.Split('\'')[7];
-                    try
-                    {
-                        ifcElements.First(x => x.ElementId.ToString()==objectId).Level=level;
-                    }
-                    catch (Exception)
-                    {
-                    }
-                  
-                }
-            }
-        }
-        void createFoundation(IfcSlab ifcSlab, Autodesk.Revit.DB.Document doc)
+        void createFoundation(IfcElement ifcSlab, Autodesk.Revit.DB.Document doc)
         {
             FamilySymbol foundationSlab = new FilteredElementCollector(doc).OfClass(typeof(FamilySymbol)).
                 OfCategory(BuiltInCategory.OST_StructuralFoundation).Cast<FamilySymbol>().
                 Where(x => x.Name==ifcSlab.TypeName).
                 Where(x => x.FamilyName == ifcSlab.FamilyName).First();
-            doc.Create.NewFamilyInstance(ifcSlab.Coordinate, foundationSlab,ifcSlab.Level, Autodesk.Revit.DB.Structure.StructuralType.Footing).LookupParameter("Mark").
+            XYZ coordinate = ifcSlab.IfcProductDefinitionShape.IfcShapeRepresentaion.IfcExtrudedAreaSolid.IfcAxis2Placement3D.IfcCatesianPoint.XYZ;
+            XYZ convertedCoordinate = new XYZ(coordinate.X._MmToFeet(), coordinate.Y._MmToFeet(),
+                coordinate.Z._MmToFeet()+foundationSlab.LookupParameter("Foundation Thickness").AsDouble());
+            doc.Create.NewFamilyInstance(convertedCoordinate, foundationSlab, ifcSlab.Level, Autodesk.Revit.DB.Structure.StructuralType.Footing).LookupParameter("Mark").
                 Set(ifcSlab.ElementId.ToString());
-     
+
         }
-        void createColumn(IfcColumn ifcColumn, Autodesk.Revit.DB.Document doc)
+        void createColumn(IfcElement ifcColumn, Autodesk.Revit.DB.Document doc)
         {
-            //Level level = new FilteredElementCollector(doc).OfClass(typeof(Level)).Cast<Level>().ToList().
-            //    First(x => x.LookupParameter("Elevation").AsDouble()-ifcColumn.Coordinate.Z==0.00);
             FamilySymbol columnFam = new FilteredElementCollector(doc).OfClass(typeof(FamilySymbol)).
                OfCategory(BuiltInCategory.OST_StructuralColumns).Cast<FamilySymbol>().
                Where(x => x.Name==ifcColumn.TypeName).
                Where(x => x.FamilyName == ifcColumn.FamilyName).First();
-            doc.Create.NewFamilyInstance(ifcColumn.Coordinate, columnFam, ifcColumn.Level, Autodesk.Revit.DB.Structure.StructuralType.Column).LookupParameter("Mark").
+            XYZ coordinate = ifcColumn.IfcProductDefinitionShape.IfcShapeRepresentaion.IfcMappedItem.IfcRepresentaionMap.IfcShapeRepresentation.IfcExtrudedAreaSolid.IfcAxis2Placement3D.IfcCatesianPoint.XYZ;
+                 XYZ convertedCoordinate = new XYZ(coordinate.X._MmToFeet(), coordinate.Y._MmToFeet(), coordinate.Z._MmToFeet());
+            doc.Create.NewFamilyInstance(convertedCoordinate, columnFam, ifcColumn.Level, Autodesk.Revit.DB.Structure.StructuralType.Column).LookupParameter("Mark").
                 Set(ifcColumn.ElementId.ToString());
         }
         void createBeam(IfcBeam ifcBeam, Autodesk.Revit.DB.Document doc)
